468428116c3ba3a5f7c0da7d756c34279dbad810
kornas1 <79769071+kornas1@users.noreply.github.com>
2021-03-12 23:21:10 +0300

    feat: add file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function toReadable (number) {
-  
+let result = '';
+    let arr1 = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+    let arr2 = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+    if (number < 20) result = arr1[number];
+    if (number > 19 && number < 100){
+        result = arr2[Math.floor(number / 10) - 2];
+        if (number % 10 != 0) result = result + ' ' + arr1[number % 10];
+    }
+    if (number >= 100 && number < 1000){
+       result = arr1[Math.floor(number / 100)];
+       if(number % 100 === 0 )
+        result = result + ' hundred';
+        if (number % 100 != 0){
+            if (number % 100 < 20)
+                result = result + ' hundred ' + arr1[number % 100];
+            else if ( number % 10 != 0 && number % 100 >= 20)
+                result = result + ' hundred ' + arr2[Math.floor((number % 100)/10) - 2] + ' ' + arr1[number % 10];
+            else result = result + ' hundred ' + arr2[Math.floor((number % 100)/10) - 2];
+        }
+    }
+    return  result;
+
 }
